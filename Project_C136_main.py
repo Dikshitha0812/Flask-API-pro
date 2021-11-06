b9243d797c5f712975a4ad70a3ae39987c73c6a0
Dikshitha0812 <71216869+Dikshitha0812@users.noreply.github.com>
2021-11-06 15:18:37 +0530

    Update Project_C136_main.py

--- Project_C136_main.py
+++ Project_C136_main.py
@@ -1,25 +1,24 @@
-from flask import Flask, jsonify, request
 from data import data
+from flask import Flask, jsonify, request
 
-app=Flask(__name__)
-@app.route("/")
+app = Flask(__name__)
+@app.route('/')
 
-def index():
-    '''The Flask API displayer'''
+def all_stars_data():
     return jsonify({
-        "data":data,
-        "message":"success"
-        })
+        'data': data,
+        'message': 'success'
+    }), 200
 
-@app.route("/planet")
-def planet():
-    '''Retreives the data for each planet'''
-    name=request.args.get("name")
-    planet_data=next(item for item in data if item["name"]==name)
+@app.route('/star')
+
+def stars_data():
+    name = request.args.get('name')
+    star_data = next(item for item in data if item[name] == name)
     return jsonify({
-        "data":planet_data,
-        "message":"success"
-        })
+        'data': star_data,
+        'message': 'success'
+    }), 200
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
